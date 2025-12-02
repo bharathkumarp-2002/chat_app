502b384a69376bfaf27972e72897b385d1a93703
bharathkumarp-2002 <bharathkumar.p2022eee@sece.ac.in>
2025-12-02 14:03:32 +0530

    Create chatController.js file with functions for creating chats, sending messages, and managing chat history

---
+++ server/controllers/chatController.js
@@ -0,0 +1,22 @@
+// chatController.js
+
+// Function to create a new chat
+function createChat(chatData) {
+    // Logic to create a new chat
+}
+
+// Function to send a message in a chat
+function sendMessage(chatId, messageData) {
+    // Logic to send a message
+}
+
+// Function to manage chat history
+function getChatHistory(chatId) {
+    // Logic to get chat history
+}
+
+module.exports = {
+    createChat,
+    sendMessage,
+    getChatHistory,
+};
